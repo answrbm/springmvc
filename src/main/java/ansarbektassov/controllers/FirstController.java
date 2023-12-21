bc38996dc8e7d4a585f48617b82f58293c897222
Ansar Bektassov <bektasovansar@gmail.com>
2023-12-21 17:02:21 +0600

    Created calculator controller with applied thymeleaf parameters

--- src/main/java/ansarbektassov/controllers/FirstController.java
+++ src/main/java/ansarbektassov/controllers/FirstController.java
@@ -1,7 +1,7 @@
 package ansarbektassov.controllers;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,13 +12,45 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-        System.out.println("Hello, " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message","Hello, " + name + " " + surname);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a") int a,
+                             @RequestParam(value = "b") int b,
+                             @RequestParam(value = "action") String action,
+                             Model model) {
+        String answer;
+        switch (action) {
+            case "multiplication" -> {
+                answer = String.valueOf(a*b);
+                action = "*";
+            }
+            case "division" -> {
+                answer = String.valueOf(a/b);
+                action = "/";
+            }
+            case "addition" -> {
+                answer = String.valueOf(a+b);
+                action = "+";
+            }
+            case "subtraction" -> {
+                answer = String.valueOf(a-b);
+                action = "-";
+            }
+            default -> {
+                answer = "Invalid operator";
+            }
+        };
+        model.addAttribute("answer","a"+action+"b="+answer);
+        return "first/calculator";
     }
 }
