3b0af0c6e7f078a9798158303bd6852c7f747419
Ansar Bektassov <ansar_bektassov@epam.com>
2023-12-22 12:29:37 +0600

    Added option to create a new person in a form(/people/new)

--- src/main/java/ansarbektassov/controllers/PeopleController.java
+++ src/main/java/ansarbektassov/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package ansarbektassov.controllers;
 
 import ansarbektassov.dao.PersonDAO;
+import ansarbektassov.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -27,6 +26,17 @@ public class PeopleController {
         //Получим одного человека из DAO и передадим на отображение на представление
         model.addAttribute("person",personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect: /springmvc/people/"+person.getId();
     }
 
 }
