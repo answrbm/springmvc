ff0665a50eac70b6da67ace967f3feecc55c3a30
Ansar Bektassov <bektasovansar@gmail.com>
2024-01-02 16:20:27 +0600

    Created admin page with select/option tag

---
+++ src/main/java/ansarbektassov/controllers/AdminController.java
@@ -0,0 +1,31 @@
+package ansarbektassov.controllers;
+
+import ansarbektassov.dao.PersonDAO;
+import ansarbektassov.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private PersonDAO personDAO;
+
+    @Autowired
+    public AdminController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping
+    public String adminPage(Model model, @ModelAttribute("person") Person person) {
+        model.addAttribute("people",personDAO.index());
+        return "adminPage";
+    }
+
+    @PatchMapping("/assign")
+    public String assignAdmin(@ModelAttribute("person")Person person) {
+        return "redirect: /springmvc/people/"+person.getId();
+    }
+}
